5e51e9b84b3346cf761f8724c3b6fb5757f5a303
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-01-26 12:26:24 +0000

    CORS Compiled

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const errors_1 = require("./errors");
 const api_router_1 = __importDefault(require("./routes/api.router"));
 const welcome_router_1 = __importDefault(require("./routes/welcome.router"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/", welcome_router_1.default);
 app.use("/api", api_router_1.default);
 app.all("/*", errors_1.handleInvalidPaths);
